408d0d6f4ef338f9c8c34b758813a68027e8bd11
ferlorca <fernando.g.lorca@gmail.com>
2016-05-31 19:08:52 -0300

    Cambio home
    Ahora el home tiene tres categorias scrolleables.

--- couchInn/resources/views/template/default/partes/nav.blade.php
+++ couchInn/resources/views/template/default/partes/nav.blade.php
@@ -1,19 +1,19 @@
-<nav class="navbar navbar-default">
-    <div class="container-fluid">
+<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+<div class="container">
         <!-- Brand and toggle get grouped for better mobile display -->
-        <div class="navbar-header">
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+    <div class="navbar-header page-scroll">
+        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
                 <span class="sr-only">Toggle navigation</span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
 
-            <a class="navbar-brand" href="/">CouchInn</a>
+        <a class="navbar-brand page-scroll" href="#page-top">CouchInn</a>
         </div>
 
         <!-- Collect the nav links, forms, and other content for toggling -->
-        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+    <div class="collapse navbar-collapse navbar-ex1-collapse">
             @if(Auth::User())
 
                 <ul class="nav navbar-nav navbar-right">
@@ -26,17 +26,37 @@
                         <li><a href="{{ url('#%^#%') }}">Mis reservas</a></li>
                         <li><a href="{{ url('#$%^#%^#^#$') }}">Mis hospedajes</a></li>
                         <li><a href="{{ url('#%^#$^3#$%^#%') }}">Editar perfil</a></li>
-                        <li><a href="{!!return view('donner.donar')!!}">Donar</a></li>  //HACER LA RUTA
+                        <li><a href="">Donar</a></li>
                         <li><a href="{{ url('/logout') }}">Salir</a></li>
 
                     </ul>
                     </li>
                 </ul>
             @else
+            <div class="collapse navbar-collapse navbar-ex1-collapse">
+                <ul class="nav navbar-nav">
+                    <!-- Hidden li included to remove active class from about link when scrolled up past about section -->
+                    <li class="hidden">
+                        <a class="page-scroll" href="#page-top"></a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#services">Anfitriones destacados</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#about">Como funciona</a>
+                    </li>
+                    <li>
+                        <a class="page-scroll" href="#contact">Mas visitados</a>
+                    </li>
+
+                </ul>
                 <ul class="nav navbar-nav navbar-right">
                     <li><a href="{{url('/login')}}">Iniciar Sesión</a></li>
                     <li><a href="{{url('/register')}}">Regístrate</a></li>
                 </ul>
+            </div>
+            <!-- /.navbar-collapse -->
+
             @endif
         </div><!-- /.navbar-collapse -->
     </div><!-- /.container-fluid -->
