873728670863e43e251a694162652721650e8687
ferlorca <fernando.g.lorca@gmail.com>
2016-06-04 15:28:21 -0300

    commit editar y crear hospedaje
    se subio crear y editar hospedaje

--- couchInn/app/Http/Controllers/HospedajeControlador.php
+++ couchInn/app/Http/Controllers/HospedajeControlador.php
@@ -11,9 +11,22 @@ use App\Imagen;
 use App\Http\Requests;
 use Laracasts\Flash\Flash;
 use Mockery\CountValidator\Exception;
+use PhpParser\Node\Stmt\TryCatch;
 
 class HospedajeControlador extends Controller
 {
+    public function edit($request){
+        $hospedaje = Hospedaje::find($request);
+        $tipo_hospedajes= Tipo_hospedaje::all();
+        $array=array();
+        foreach ($tipo_hospedajes as $tipo_hospedaje){
+            $array[$tipo_hospedaje->id]=$tipo_hospedaje->tipo;        }
+
+        return view('template.default.Hospedaje.edit')
+            ->with('tipo_hospedajes',$array)
+            ->with('hospedaje',$hospedaje);
+    }
+
     public function create(){
         $tipo_hospedajes =Tipo_hospedaje::all();
         $array=array();
@@ -27,11 +40,8 @@ class HospedajeControlador extends Controller
             ->with('tipo_hospedajes',$array);
     }
 
-    public function update(){
-        return view('template.default.Hospedaje.edit');
-    }
-
     public function store(Request $request){
+
         try{
             $hospedaje= new Hospedaje;
             $hospedaje->fill($request->all());
@@ -59,4 +69,30 @@ class HospedajeControlador extends Controller
         Flash::success('se ha agregado el hospedaje con exito');
         return redirect()->route('usuario.perfil.index');
     }
+
+
+    public function update(Request $request,$id){
+        $hospedaje=Hospedaje::find($id);
+        try{
+            $hospedaje->fill($request->all());
+            $hospedaje->tipo_hospedaje_id=$request->tipo_hospedaje;
+            $hospedaje->save();
+            if($request->file('imagen')!=null){
+                $file=$request->file('imagen');
+                $name='Imagen_'.Auth::User()->id.'_'.time().'.'.$file->getClientOriginalExtension();
+                $path=public_path().'/images/hospedajes';
+                $file->move($path,$name);
+                //solo se cambia la primera imagen porq no podemos meter mas todabia
+                $imagen=Imagen::find($hospedaje->imagenes->first()->id);
+                $imagen->nombre=$name;
+                $imagen->save();
+            }
+        }catch (Exception $e){
+            dd('error al guardar los cambios'.$e);
+        }
+
+        Flash::success('se ha agregado el hospedaje con exito');
+        return redirect()->route('usuario.perfil.index');
+    }
+
 }
